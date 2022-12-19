fc949639c2bcd0d3d9720b219a0a5a3a4b4509d4
f4nn-x <francis.beatriz.bb@gmail.com>
2022-12-19 18:11:44 -0300

    initial commit

---
+++ src/application/repositories/notification-respository.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.NotificationsRepository = void 0;
+var NotificationsRepository = /** @class */ (function () {
+    function NotificationsRepository() {
+    }
+    return NotificationsRepository;
+}());
+exports.NotificationsRepository = NotificationsRepository;
+;
